42ed77effa41852a8aec6f8cfbcb714be4f45a21
SanaranjoSRV <developer.srv.b01@gmail.com>
2025-11-21 14:27:23 -0500

    integracion pruebas unitarias v.5

--- test/integration/messages.integration.test.js
+++ test/integration/messages.integration.test.js
@@ -7,9 +7,9 @@ app.use(express.json());
 app.use('/api', messageRoutes);
 
 describe('GET /api/messages', () => {
-  it('should return all messages', async () => {
-    const res = await request(app).get('/api/messages');
-    expect(res.statusCode).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
-  });
+    it('should return all messages', async () => {
+        const response = await request(app).get('/api/messages');
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
 });
